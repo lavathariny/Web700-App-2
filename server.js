2296e74674848580cef620fc01d6d8bf35afa706
Jasinthakumar Arumugam <ljasinthakumar@myseneca.ca>
2024-07-25 19:12:07 -0400

    make it better

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 /*********************************************************************************
-*  WEB700 – Assignment 04
+*  WEB700 – Assignment 05
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part 
 *  of this assignment has been copied manually or electronically from any other source 
 *  (including 3rd party web sites) or distributed to other students.
 * 
-*  Name: Lavatharini Jasinthakumar Student ID: 153494232 Date: July 18, 2024
+*  Name: Lavatharini Jasinthakumar Student ID: 153494232 Date: July 25, 2024
 *
 * Online (Heroku) Link: ________________________________________________________
 *
